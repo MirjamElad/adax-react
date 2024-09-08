f2b14d54716514c8146cf8528f4b184a8982eff4
MirjamElad <mirjamelad@gmail.com>
2024-09-08 15:50:10 +0200

    code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import {useMemo, useEffect, useState, useRef} from "react";
-import { subscribe, type QueryOptions as QO, type Result, getSortedID, addQuery, removeQuery } from 'adax-core';
+import { useEffect, useState, useRef } from "react";
+import { subscribe, type QueryOptions as QO, type Result } from 'adax-core';
 
 
 export interface QueryOptions extends QO {
@@ -31,7 +31,7 @@ const getDependencyArray = (readFn: Function, paramsObj?: any, options: QueryOpt
   });
   return ret;
 }
-// IAK 6!
+
 export const useSync = <FnType extends (x: any) => any>(
   readFn: FnType,
   paramsObj?: Parameters<FnType>[0],
@@ -43,7 +43,6 @@ export const useSync = <FnType extends (x: any) => any>(
   const paramsArr = getDependencyArray(readFn, paramsObj, options);
   const readTrigger = (res: Result) => {
     result.current = res.data;
-    //console.info('result.current is now: ', res);
     forceUpdate((d) => d + 1);
   };
   useEffect(() => {
@@ -62,62 +61,3 @@ export const useSync = <FnType extends (x: any) => any>(
   }, paramsArr);
   return result.current;
 };
-
-
-// Extra comment to force new version 0.0.6
-// export const useSync = <FnType extends (x: any) => any>(
-//   readFn: FnType,
-//   paramsObj?: Parameters<FnType>[0],
-//   options: QueryOptions = {}
-// ) => {
-//   const initResult = useRef<{ data: ReturnType<FnType>, prevData: ReturnType<FnType> | undefined, version: number }>({
-//     data: readFn(paramsObj || undefined),
-//     prevData: undefined,
-//     version: 0,
-//   });
-//   const [_, forceUpdate] = useState<number>(0);
-//   const cmpId = useRef(options.cmpId ?? getSortedID());
-//   const paramsArr = paramsObj
-//     ? Object.keys(paramsObj).map((k) => paramsObj[k])
-//     : [];
-//   //paramsArr.push(Kernel.read2writeListMap?.has(readFn));
-//   useEffect(() => {
-//     // Better useEffect ? https://stackoverflow.com/questions/54095994/react-useeffect-comparing-objects
-//     addQuery({ queryFn: readFn, queryInstance: {
-//       instanceKey: cmpId.current,
-//       readTrigger: (res: Result) => {
-//         initResult.current = res;
-//         forceUpdate((d) => d + 1);
-//       },
-//       paramsObj: paramsObj || {},
-//       options,
-//       //TODO: Make sure below is correct (Trying to have a valid prevData before the first write)
-//       result: initResult.current
-//     }});
-//     return () => {
-//       removeQuery({ queryFn: readFn, queryInstance: {
-//         // eslint-disable-next-line 
-//         instanceKey: cmpId.current,
-//         readTrigger: undefined,
-//         paramsObj: paramsObj || {},
-//         options,
-//         result: { data: undefined, prevData: undefined, version: 0 }
-//       }});
-//     };
-//     // eslint-disable-next-line 
-//   }, paramsArr);
-//   useMemo(
-//     () => {
-//       initResult.current.data =
-//         initResult.current.data || readFn(paramsObj || undefined);
-//     },
-//     // eslint-disable-next-line 
-//     paramsArr
-//   );
-//   const { data, version, prevData } = initResult.current;
-//   return {
-//     data,
-//     version,
-//     prevData
-//   };
-// };
